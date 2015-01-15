1b939043a1c0638f64b60881d3b2ec7d9a449b33
Paul Reioux <reioux@gmail.com>
2015-01-15 03:01:02 -0600

    Simple gpu algorithm added

---
+++ build.sh
@@ -0,0 +1,15 @@
+export CROSS_COMPILE=/home/ajith/8916/UBERTC-aarch64-linux-android-4.9-new/bin/aarch64-linux-android-
+export ARCH=arm64
+export SUBARCH=arm64
+export USE_CCACHE=1
+#make menuconfig
+make cyanogenmod_tomato-64_defconfig
+<<<<<<< HEAD
+make -j8
+=======
+<<<<<<< HEAD
+make -j5
+=======
+make -j8
+>>>>>>> 1b3a355... Simple gpu algorithm added
+>>>>>>> 9b0c06d... Simple gpu algorithm added
