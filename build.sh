3ec398ba824b8440ad68eabdf970d95ec0558435
anburajajith <anburajajith@gmail.com>
2016-07-28 15:21:13 +0530

    disabled warning

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-export CROSS_COMPILE=/home/ajith/8916/gcc-linaro-5.3-2016.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=/home/ajith/8916/UBERTC-aarch64-linux-android-5.3/bin/aarch64-linux-android-
 export ARCH=arm64
 export SUBARCH=arm64
 export USE_CCACHE=1
 #make menuconfig
 #./dtbTool -2 -o ./arch/arm64/boot/dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/dts/
-make cyanogenmod_tomato-64_defconfig
+make kali_defconfig
 make -j5
 
 
