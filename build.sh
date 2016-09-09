35af5bb41208a86f12efbbdf6fec3c0b7d8dc86e
anburajajith <anburajajith@gmail.com>
2016-09-09 10:32:07 +0530

    Minor fixes

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
-export CROSS_COMPILE=/home/ajith/8916/UBERTC-aarch64-linux-android-5.3/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/ajith/8916/UBERTC-aarch64-linux-android-6.0/bin/aarch64-linux-android-
 export ARCH=arm64
 export SUBARCH=arm64
 export USE_CCACHE=1
+export KBUILD_BUILD_USER=Ajith
+export KBUILD_BUILD_HOST=Xzotic
+AK_VER="Despair.M.CFS.R1.Shamu saber"
+echo "$AK_VER"
+export LOCALVERSION=~`echo $AK_VER`
 #make menuconfig
-#./dtbTool -2 -o ./arch/arm64/boot/dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/dts/
 make kali_defconfig
 make -j5
+#./dtbTool -2 -o ./arch/arm64/boot/dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/dts/
 
 
+#git fetch <remote-git-url> <branch> && git cherry-pick FETCH_HEAD
